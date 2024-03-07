a35970b0c114fec0c1195ec67f8db307fadf45c5
benjaminnn00 <161681244+benjaminnn00@users.noreply.github.com>
2024-03-07 09:30:20 +0545

    Create prime_number.java

---
+++ prime_number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class prime_number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Enter a number:");
+        number = scanner.nextInt();
+        int a = 0;
+        for(int i = 2;i<=number-1;i++){
+            if(number %i ==0){
+                a=1;
+                break;
+            }
+        }
+        if(number==1){
+            System.out.println("1 is neither prime nor composite");
+
+        }
+        else if(a==0){
+            System.out.println("The given number is prime");
+        }
+        else{
+            System.out.println("The given number is composite");
+        }
+    }
+}
+sum_of_two.java
